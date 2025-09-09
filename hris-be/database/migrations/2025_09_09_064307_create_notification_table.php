e1cd689fd4742d77124c5fac727aca0bd570b6fb
karriv18 <krlclarence@gmail.com>
2025-09-09 16:42:36 +0800

    creating asynchronous notification

---
+++ hris-be/database/migrations/2025_09_09_064307_create_notification_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Employee;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->id();
+            $table->string('message');
+            $table->unsignedBigInteger('employee_id');
+            $table->foreign('employee_id')
+                    ->references('id')
+                    ->on('employee')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notification');
+    }
+};
